27b7cb78769354927b33bb203f00ccc4b3051ec6
Luiz Fernando <f3rn.luiz@gmail.com>
2024-09-07 15:25:06 -0300

    docs(Ionic Project): iniciando projeto ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'time-task-app',
+  webDir: 'www'
+};
+
+export default config;
