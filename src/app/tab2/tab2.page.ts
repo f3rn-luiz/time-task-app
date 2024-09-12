c37fad0a34630c8379f3537242fe1b7798479b8e
Luiz Fernando <f3rn.luiz@gmail.com>
2024-09-11 23:43:22 -0300

    docs(Prettier): formatação e padronização do projeto

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
