7fab2205a8d47655feefdb88be46900b3ec65926
Luiz Fernando <f3rn.luiz@gmail.com>
2024-10-24 17:41:18 -0300

    docs(Main): definição do padrão para pt-BR

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,3 +1,4 @@
+import { LOCALE_ID } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -5,5 +6,5 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideIonicAngular(), provideRouter(routes, withPreloading(PreloadAllModules))],
+	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: LOCALE_ID, useValue: 'pt-BR' }, provideIonicAngular(), provideRouter(routes, withPreloading(PreloadAllModules))],
 });
