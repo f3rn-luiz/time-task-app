d62bdf2212d6d6cbbc2064b2719cd46f9e1c8855
Luiz Fernando <f3rn.luiz@gmail.com>
2024-10-25 19:02:59 -0300

    feat(Calendario): incremento de complexidade nos objetos e estilos

--- app/src/app/pages/calendario/calendario.page.ts
+++ app/src/app/pages/calendario/calendario.page.ts
@@ -10,7 +10,46 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CalendarioPage {
 	hoje = new Date();
-	mes: string[][] = [[]];
+	data_selecionada = this.hoje;
+
+	mes: any[][] = [[]];
+
+	sem_comeca_segunda = false;
+	semana = {
+		domingo: [
+			{ letra: 'D', simplificado: 'Dom', completo: 'Domingo' },
+			{ letra: 'S', simplificado: 'Seg', completo: 'Segunda-feira' },
+			{ letra: 'T', simplificado: 'Ter', completo: 'Terça-feira' },
+			{ letra: 'Q', simplificado: 'Qua', completo: 'Quarta-feira' },
+			{ letra: 'Q', simplificado: 'Qui', completo: 'Quinta-feira' },
+			{ letra: 'S', simplificado: 'Sex', completo: 'Sexta-feira' },
+			{ letra: 'S', simplificado: 'Sab', completo: 'Sábado' },
+		],
+		segunda: [
+			{ letra: 'S', simplificado: 'Seg', completo: 'Segunda-feira' },
+			{ letra: 'T', simplificado: 'Ter', completo: 'Terça-feira' },
+			{ letra: 'Q', simplificado: 'Qua', completo: 'Quarta-feira' },
+			{ letra: 'Q', simplificado: 'Qui', completo: 'Quinta-feira' },
+			{ letra: 'S', simplificado: 'Sex', completo: 'Sexta-feira' },
+			{ letra: 'S', simplificado: 'Sab', completo: 'Sábado' },
+			{ letra: 'D', simplificado: 'Dom', completo: 'Domingo' },
+		],
+	};
+
+	meses = [
+		{ simplificado: 'Jan', completo: 'Janeiro' },
+		{ simplificado: 'Fev', completo: 'Fevereiro' },
+		{ simplificado: 'Mar', completo: 'Março' },
+		{ simplificado: 'Abr', completo: 'Abril' },
+		{ simplificado: 'Mai', completo: 'Maio' },
+		{ simplificado: 'Jun', completo: 'Junho' },
+		{ simplificado: 'Jul', completo: 'Julho' },
+		{ simplificado: 'Ago', completo: 'Agosto' },
+		{ simplificado: 'Set', completo: 'Setembro' },
+		{ simplificado: 'Out', completo: 'Outubro' },
+		{ simplificado: 'Nov', completo: 'Novembro' },
+		{ simplificado: 'Dez', completo: 'Dezembro' },
+	];
 
 	constructor() {
 		console.log('hoje: ' + this.hoje);
@@ -18,8 +57,8 @@ export class CalendarioPage {
 	}
 
 	ordenarDias() {
-		let primeiro_dia = new Date(`${this.hoje.getMonth() + 1}/01/${this.hoje.getFullYear()}`);
-		let ultimo_dia: Date | number = new Date(`${this.hoje.getMonth() + 1 === 12 ? 1 : this.hoje.getMonth() + 2}/01/${this.hoje.getFullYear()}`);
+		let primeiro_dia = new Date(`${this.data_selecionada.getMonth() + 1}/01/${this.data_selecionada.getFullYear()}`);
+		let ultimo_dia: Date | number = new Date(`${this.data_selecionada.getMonth() + 1 === 12 ? 1 : this.data_selecionada.getMonth() + 2}/01/${this.data_selecionada.getFullYear()}`);
 		ultimo_dia.setHours(23, 59, 59);
 		ultimo_dia = new Date(ultimo_dia.getTime() - 86400000);
 
@@ -29,11 +68,23 @@ export class CalendarioPage {
 		let dia_semana = primeiro_dia.getDay();
 		let semana = 0;
 
-		for (let idx = 1; idx <= ultimo_dia.getDate(); idx++) {
-			if (idx === 1 && dia_semana !== 0) for (let si = 0; si < dia_semana; si++) this.mes[0][si] = '-';
-			else if (idx === ultimo_dia.getDate() && ultimo_dia.getDay() !== 6) for (let si = ultimo_dia.getDay(); si <= 6; si++) this.mes[semana][si] = '-';
+		console.log('dia_semana: ', dia_semana);
 
-			this.mes[semana][dia_semana] = `${idx}`;
+		for (let idx = 1; idx <= ultimo_dia.getDate(); idx++) {
+			if (idx === 1 && dia_semana !== 0) {
+				let n = new Date(primeiro_dia.getTime() - 86400000).getDate() - (dia_semana - 1);
+				for (let si = 0; si < dia_semana; si++, n++) {
+					this.mes[0][si] = { dia: n, atual: false, selecionado: false, hoje: false, domingo: si === 0 };
+				}
+			} else if (idx === ultimo_dia.getDate() && ultimo_dia.getDay() !== 6) {
+				let n = 0;
+				for (let si = ultimo_dia.getDay(); si <= 6; si++, n++) {
+					this.mes[semana][si] = { dia: n, atual: false, selecionado: false, hoje: false, domingo: false };
+				}
+			}
+
+			let isHoje = this.data_selecionada.getMonth() === this.hoje.getMonth() && this.data_selecionada.getFullYear() === this.hoje.getFullYear() && idx === this.hoje.getDate();
+			this.mes[semana][dia_semana] = { dia: idx, atual: true, selecionado: isHoje, hoje: isHoje, domingo: dia_semana === 0 };
 
 			if (dia_semana === 6 && idx !== ultimo_dia.getDate()) {
 				dia_semana = 0;
