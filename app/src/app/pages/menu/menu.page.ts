aa02a4740b0faf1a3f1af996580dac169d7c8044
Luiz Fernando <f3rn.luiz@gmail.com>
2024-11-24 09:44:39 -0300

    docs(Menu): remoção do IonicModule por imports individuais

--- app/src/app/pages/menu/menu.page.ts
+++ app/src/app/pages/menu/menu.page.ts
@@ -1,11 +1,11 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
 
 @Component({
 	selector: 'app-menu',
 	templateUrl: 'menu.page.html',
 	standalone: true,
-	imports: [IonicModule],
+	imports: [IonLabel, IonIcon, IonTabBar, IonTabButton, IonTabs],
 })
 export class MenuPage {
 	public environmentInjector = inject(EnvironmentInjector);
