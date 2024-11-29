7740d434f34de93fed26783856def222c78e7fa4
Luiz Fernando <f3rn.luiz@gmail.com>
2024-11-29 15:12:02 -0300

    docs(Main): registro do elemento do swiper na main

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,8 +2,11 @@ import { LOCALE_ID } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { register } from 'swiper/element/bundle';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+
+register();
 
 bootstrapApplication(AppComponent, {
 	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: LOCALE_ID, useValue: 'pt-BR' }, provideIonicAngular(), provideRouter(routes, withPreloading(PreloadAllModules))],
