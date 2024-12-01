da4f291881bc35159cbd29b14104aff0837a1557
Luiz Fernando <f3rn.luiz@gmail.com>
2024-12-01 11:57:00 -0300

    feat(Calendario): mudança de abordagem, remoção da antiga e criação da no

--- app/src/app/pages/calendario/calendario.page.ts
+++ app/src/app/pages/calendario/calendario.page.ts
@@ -34,59 +34,10 @@ export class CalendarioPage implements OnInit {
 	}
 
 	ngOnInit(): void {
-		this.eventosSwiper();
+		this.iniciarEventosSwiper();
 	}
 
-	diaSelecionado(data: any): boolean {
-		if (data.atual && data.dia === this.data_selecionada.getDate()) return true;
-		return false;
-	}
-
-	ordenarDias() {
-		let primeiro_dia = new Date(`${this.data_selecionada.getMonth() + 1}/01/${this.data_selecionada.getFullYear()}`);
-		let ultimo_dia: Date | number = new Date(`${this.data_selecionada.getMonth() + 1 === 12 ? 1 : this.data_selecionada.getMonth() + 2}/01/${this.data_selecionada.getFullYear()}`);
-		ultimo_dia.setHours(23, 59, 59);
-		ultimo_dia = new Date(ultimo_dia.getTime() - 86400000);
-
-		console.log('PD: ', primeiro_dia);
-		console.log('UD: ', ultimo_dia);
-
-		let dia_semana = primeiro_dia.getDay();
-		let semana = 0;
-
-		console.log('dia_semana: ', dia_semana);
-
-		for (let idx = 1; idx <= ultimo_dia.getDate(); idx++) {
-			if (idx === 1 && dia_semana !== 0) {
-				let n = new Date(primeiro_dia.getTime() - 86400000).getDate() - (dia_semana - 1);
-				for (let si = 0; si < dia_semana; si++, n++) {
-					this.mes[0][si] = { dia: n, atual: false, hoje: false, domingo: si === 0 };
-				}
-			} else if (idx === ultimo_dia.getDate() && ultimo_dia.getDay() !== 6) {
-				let n = 0;
-				for (let si = ultimo_dia.getDay(); si <= 6; si++, n++) {
-					this.mes[semana][si] = { dia: n, atual: false, hoje: false, domingo: false };
-				}
-			}
-
-			let isHoje = this.data_selecionada.getMonth() === this.hoje.getMonth() && this.data_selecionada.getFullYear() === this.hoje.getFullYear() && idx === this.hoje.getDate();
-			this.mes[semana][dia_semana] = { dia: idx, atual: true, hoje: isHoje, domingo: dia_semana === 0 };
-
-			if (dia_semana === 6 && idx !== ultimo_dia.getDate()) {
-				dia_semana = 0;
-				semana++;
-				this.mes.push([]);
-			} else dia_semana++;
-		}
-		console.log('DIAS: ', this.mes);
-	}
-
-	selecionarData(data: any) {
-		console.log('CLICK: ', data);
-		if (data.atual && data.dia !== this.data_selecionada.getDate()) this.data_selecionada = new Date(`${this.data_selecionada.getMonth() + 1}/${data.dia}/${this.data_selecionada.getFullYear()}`);
-	}
-
-	eventosSwiper() {
+	iniciarEventosSwiper() {
 		const swiper = document.querySelector('swiper-container')!.swiper;
 		swiper.on('slideChangeTransitionEnd', () => {
 			swiper.allowTouchMove = false;
