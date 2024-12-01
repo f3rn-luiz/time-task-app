d8952328d6bfbfcfecfed19fa975d5f60d5004e5
Luiz Fernando <f3rn.luiz@gmail.com>
2024-12-01 11:57:56 -0300

    feat(Calendario Type): criação da tipagem para o calendário

---
+++ app/src/app/core/calendario/calendario.type.ts
@@ -0,0 +1,22 @@
+export interface DiaSemana {
+	letra: string;
+	simplificado: string;
+	completo: string;
+}
+
+export interface MesAno {
+	simplificado: string;
+	completo: string;
+}
+
+export interface Dia {
+	dia: number;
+	dia_semana: number; // 0 a 6
+	mes_atual: boolean;
+}
+
+export interface Mes {
+	mes: number; // 0 a 11
+	dias: Dia[];
+	ano: number;
+}
