ddb3992be10076c04c457935a8c47a0d1513c68e
Luiz Fernando <f3rn.luiz@gmail.com>
2025-02-09 11:09:00 -0300

    feat(Tarefas): pré população da página tarefas com as guias principais

--- app/src/app/pages/tarefas/tarefas.page.ts
+++ app/src/app/pages/tarefas/tarefas.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonIcon, IonRippleEffect, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
 	selector: 'app-tarefas',
 	templateUrl: 'tarefas.page.html',
 	standalone: true,
-	imports: [IonContent, IonTitle, IonToolbar, IonHeader],
+	imports: [IonIcon, IonRippleEffect, IonContent, IonTitle, IonToolbar, IonHeader],
 })
 export class TarefasPage {
 	constructor() {}
