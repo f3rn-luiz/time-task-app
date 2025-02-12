d05bea3429893fb5b07ca2602cb15332e44d51ca
Luiz Fernando <f3rn.luiz@gmail.com>
2025-02-12 17:05:00 -0300

    feat(Tarefas Hoje): criação da pagina e pré implementação de estilo

--- app/src/app/pages/tarefas/hoje/tarefas-hoje.page.ts
+++ app/src/app/pages/tarefas/hoje/tarefas-hoje.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonIcon, IonRippleEffect, NavController } from '@ionic/angular/standalone';
+import { DataSimples, dia_semana, meses } from 'src/app/core/calendario/calendario.type';
 
 @Component({
 	selector: 'app-tarefas-hoje',
 	templateUrl: 'tarefas-hoje.page.html',
+	styleUrl: 'tarefas-hoje.page.scss',
 	standalone: true,
-	imports: [IonContent, IonTitle, IonToolbar, IonHeader],
+	imports: [IonRippleEffect, IonIcon, IonContent],
 })
 export class TarefasHojePage {
-	constructor() {}
+	data_hoje: Date = new Date();
+	data_simples_hoje: DataSimples = { dia: this.data_hoje.getDate(), mes: this.data_hoje.getMonth(), ano: this.data_hoje.getFullYear() };
+	data_hoje_formatada: string = `${dia_semana[this.data_hoje.getDay()].completo}, ${this.data_simples_hoje.dia} de ${meses[this.data_simples_hoje.mes].completo}`;
+
+	constructor(private navController: NavController) {}
+
+	voltarParaTarefas = () => this.navController.navigateBack('tarefas');
 }
